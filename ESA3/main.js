c2e27df6019a74d59073b95ce92dfbaf82139a43
gbensch00 <gbensch00@gmail.com>
2022-01-09 10:27:15 +0100

    ESA3 1.1

--- ESA3/main.js
+++ ESA3/main.js
@@ -3,19 +3,19 @@ var appStates = {
   RUNNING: "RUNNING",
   STEP: "STEP",
   STOPPED: "STOPPED",
-}
+};
 var speed = {
   Langsam: "5000",
   Normal: "1000",
   Schnell: "500",
-}
-var appState = appStates.STOPPED
-var currentSpeed = speed.Normal
-var stepWord = "foo"
+};
+var appState = appStates.STOPPED;
+var currentSpeed = speed.Normal;
+var stepWord = "foo";
 
-var pauseRequested = false
-var currentStep = 0
-var aut = null
+var pauseRequested = false;
+var currentStep = 0;
+var aut = null;
 
 async function tourClick() {
   introJs()
@@ -84,144 +84,145 @@ async function tourClick() {
         },
       ],
     })
-    .start()
+    .start();
 }
 
 async function playClick() {
-  pauseRequested = false
-  var lastNode = "q0"
-  var lastEdge = ""
-  appState = "RUNNING"
-  setupButtonStates()
+  pauseRequested = false;
+  var lastNode = "q0";
+  var lastEdge = "";
+  appState = "RUNNING";
+  setupButtonStates();
 
-  resetColors()
-  var input = document.getElementById("input_expression").value.trim()
-  aut = new Automaton(input)
-  var fin = false
+  resetColors();
+  var input = document.getElementById("input_expression").value.trim();
+  aut = new Automaton(input);
+  var fin = false;
 
   document
     .getElementById("node_" + aut.currentState.name)
-    .classList.toggle("green")
-  var res = aut.nextState()
+    .classList.toggle("green");
+  var res = aut.nextState();
   while (res.result && fin == false) {
     await new Promise((r) =>
       setTimeout(r, document.getElementById("drp-speed").value)
-    )
-    document.getElementById("node_" + lastNode).classList.toggle("green")
+    );
+    document.getElementById("node_" + lastNode).classList.toggle("green");
     if (lastEdge.length > 0) {
-      document.getElementById("t_" + lastEdge).classList.toggle("green")
-      document.getElementById("r_" + lastEdge).classList.toggle("green")
-      console.log("r_" + lastEdge)
+      document.getElementById("t_" + lastEdge).classList.toggle("green");
+      document.getElementById("r_" + lastEdge).classList.toggle("green");
+      console.log("r_" + lastEdge);
     }
 
-    lastNode = aut.currentState.name
-    lastEdge = res.rule
+    lastNode = aut.currentState.name;
+    lastEdge = res.rule;
     if (lastEdge == "q2_q3_1") {
-      lastEdge = "q2_q3_0"
+      lastEdge = "q2_q3_0";
     }
 
     if (lastEdge == "q3_q2_1") {
-      lastEdge = "q3_q2_0"
+      lastEdge = "q3_q2_0";
     }
-    console.log(lastEdge)
-    document.getElementById("node_" + lastNode).classList.toggle("green")
+    console.log(lastEdge);
+    document.getElementById("node_" + lastNode).classList.toggle("green");
     if (lastEdge.length > 0) {
-      console.log("r_" + lastEdge)
+      console.log("r_" + lastEdge);
 
-      document.getElementById("t_" + lastEdge).classList.toggle("green")
-      document.getElementById("r_" + lastEdge).classList.toggle("green")
+      document.getElementById("t_" + lastEdge).classList.toggle("green");
+      document.getElementById("r_" + lastEdge).classList.toggle("green");
     }
-    if (aut.finished()) fin = true
-    res = aut.nextState()
+    if (aut.finished()) fin = true;
+    res = aut.nextState();
   }
-  resetColors()
+  resetColors();
   if (fin) {
-    tryAddClass("node_" + lastNode, "green")
-    tryAddClass("r_" + lastEdge, "green", "pulsing")
-    tryAddClass("t_" + lastEdge, "green", "pulsing")
+    tryAddClass("node_" + lastNode, "green");
+    tryAddClass("r_" + lastEdge, "green", "pulsing");
+    tryAddClass("t_" + lastEdge, "green", "pulsing");
   } else {
-    tryAddClass("node_" + lastNode, "red")
-    tryAddClass("r_" + lastEdge, "red", "pulsing")
-    tryAddClass("t_" + lastEdge, "red", "pulsing")
+    tryAddClass("node_" + lastNode, "red");
+    tryAddClass("r_" + lastEdge, "red", "pulsing");
+    tryAddClass("t_" + lastEdge, "red", "pulsing");
   }
-  appState = "STOPPED"
-  setupButtonStates(true)
+  appState = "STOPPED";
+  document.getElementById("btn-cancel").classList.add("css-tooltip");
+  setupButtonStates(true);
 }
-var lastStepNode = "q0"
-var lastStepEdge = ""
+var lastStepNode = "q0";
+var lastStepEdge = "";
 
 function stepClick() {
   if (appState == "STOPPED") {
-    appState = "STEP"
-    setupButtonStates()
-    var input = document.getElementById("input_expression").value.trim()
-    aut = new Automaton(input)
-    resetColors()
-    document.getElementById("node_q0").classList.toggle("green")
+    appState = "STEP";
+    setupButtonStates();
+    var input = document.getElementById("input_expression").value.trim();
+    aut = new Automaton(input);
+    resetColors();
+    document.getElementById("node_q0").classList.toggle("green");
   }
 
-  var res = aut.nextState()
+  var res = aut.nextState();
 
-  document.getElementById("node_" + lastStepNode).classList.toggle("green")
+  document.getElementById("node_" + lastStepNode).classList.toggle("green");
   if (lastStepEdge.length > 0) {
-    document.getElementById("t_" + lastStepEdge).classList.toggle("green")
-    document.getElementById("r_" + lastStepEdge).classList.toggle("green")
+    document.getElementById("t_" + lastStepEdge).classList.toggle("green");
+    document.getElementById("r_" + lastStepEdge).classList.toggle("green");
   }
 
-  lastStepNode = aut.currentState.name
-  lastStepEdge = res.rule
+  lastStepNode = aut.currentState.name;
+  lastStepEdge = res.rule;
   if (lastEdge == "q2_q3_1") {
-    lastEdge = "q2_q3_0"
+    lastEdge = "q2_q3_0";
   }
 
   if (lastEdge == "q3_q2_1") {
-    lastEdge = "q3_q2_0"
+    lastEdge = "q3_q2_0";
   }
-  document.getElementById("node_" + lastStepNode).classList.toggle("green")
+  document.getElementById("node_" + lastStepNode).classList.toggle("green");
   if (lastStepEdge.length > 0) {
-    console.log("r_" + lastStepEdge)
+    console.log("r_" + lastStepEdge);
 
-    document.getElementById("t_" + lastStepEdge).classList.toggle("green")
-    document.getElementById("r_" + lastStepEdge).classList.toggle("green")
+    document.getElementById("t_" + lastStepEdge).classList.toggle("green");
+    document.getElementById("r_" + lastStepEdge).classList.toggle("green");
   }
-  var fin = false
+  var fin = false;
   if (aut.word.length == 0) {
-    fin = aut.finished()
+    fin = aut.finished();
   } else {
-    return
+    return;
   }
 
   if (fin) {
-    document.getElementById("input_expression").value = ""
-    alert("Erfolgreich beendet")
+    document.getElementById("input_expression").value = "";
+    alert("Erfolgreich beendet");
 
-    document.getElementById("node_" + lastStepNode).classList.add("green")
+    document.getElementById("node_" + lastStepNode).classList.add("green");
     document
       .getElementById("t_" + lastStepEdge)
-      .classList.add("green", "pulsing")
+      .classList.add("green", "pulsing");
     document
       .getElementById("r_" + lastStepEdge)
-      .classList.add("green", "pulsing")
-    lastStepEdge = ""
-    lastStepNode = "q0"
-    appState = "STOPPED"
-    setupButtonStates(true)
+      .classList.add("green", "pulsing");
+    lastStepEdge = "";
+    lastStepNode = "q0";
+    appState = "STOPPED";
+    setupButtonStates(true);
   } else {
-    alert("Mit fehler beendet!")
-    document.getElementById("input_expression").value = ""
-    document.getElementById("node_" + lastStepNode).classList.add("red")
+    alert("Mit fehler beendet!");
+    document.getElementById("input_expression").value = "";
+    document.getElementById("node_" + lastStepNode).classList.add("red");
     if (lastStepEdge.length > 0) {
       document
         .getElementById("t_" + lastStepEdge)
-        .classList.add("red", "pulsing")
+        .classList.add("red", "pulsing");
       document
         .getElementById("r_" + lastStepEdge)
-        .classList.add("red", "pulsing")
+        .classList.add("red", "pulsing");
     }
-    lastStepEdge = ""
-    lastStepNode = "q0"
-    appState = "STOPPED"
-    setupButtonStates(true)
+    lastStepEdge = "";
+    lastStepNode = "q0";
+    appState = "STOPPED";
+    setupButtonStates(true);
   }
 }
 
@@ -229,24 +230,24 @@ function stepClick() {
  
 */
 function genValid() {
-  appState = "RUNNING"
-  setupButtonStates()
+  appState = "RUNNING";
+  setupButtonStates();
   document.getElementById("input_expression").value =
-    new Generator().generateValid()
-  appState = "STOPPED"
-  setupButtonStates(true)
+    new Generator().generateValid();
+  appState = "STOPPED";
+  setupButtonStates(true);
 }
 
 function genInvalid() {
-  appState = "RUNNING"
-  setupButtonStates()
+  appState = "RUNNING";
+  setupButtonStates();
   document.getElementById("input_expression").value =
-    new Generator().generateInvalid()
-  appState = "STOPPED"
-  setupButtonStates(true)
+    new Generator().generateInvalid();
+  appState = "STOPPED";
+  setupButtonStates(true);
 }
 
 window.onload = (event) => {
-  appState = "STOPPED"
-  setupButtonStates()
-}
+  appState = "STOPPED";
+  setupButtonStates();
+};
