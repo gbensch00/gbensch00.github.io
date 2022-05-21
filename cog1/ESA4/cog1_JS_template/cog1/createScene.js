bd41607589e05ac47a06dfbd2c1d805057daa138
gbensch00 <gbensch00@gmail.com>
2022-05-21 13:22:26 +0200

    ESA4

---
+++ cog1/ESA4/cog1_JS_template/cog1/createScene.js
@@ -0,0 +1,35 @@
+/**
+ * Populate the scene-graph with nodes,
+ * calling methods form the scene-graph and node modules.
+ * 
+ * Texture files have to exist in the "textures" sub-directory.
+ * 
+ * @namespace cog1
+ * @module createScene
+ */
+define(["exports", "scenegraph", "animation"], //
+	function(exports, scenegraph, animation) {
+		"use strict";
+
+		/**
+		 * 	Call methods form the scene-graph (tree) module to create the scene.
+		 *
+		 */
+		function init() {
+
+			var cubeNode = scenegraph.createNodeWithModel("insideOutPoly", "insideOutPoly", {
+				scale: 1
+			});
+			cubeNode.rotateTo([0.19, -3.2, 0]);
+
+			// BEGIN exercise myModel
+
+			// END exercise myModel
+
+			return;
+
+		}
+
+		// Public API.
+		exports.init = init;
+	});
